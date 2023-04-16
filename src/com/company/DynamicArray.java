237ae24d98e6b778003a90a1122c883242b70d45
auronus <auronus@mail.ru>
2023-04-16 18:11:40 +0300

    Review fixes; Add some methods (not all)

--- src/com/company/DynamicArray.java
+++ src/com/company/DynamicArray.java
@@ -1,51 +1,52 @@
 package com.company;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
-public class DynamicArray<T> implements List {
-    //ArrayList
-    private int size = 16;
-    private int actualSize = 0;
+public class DynamicArray<T> implements List<T> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final Object[] DEFAULT_CAPACITY_EMPTY_ARRAY = {};
+    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
+
+    private int capacity = 16;
+    private int size = 0;
     private Object[] array;
 
     public DynamicArray() {
-        array = new Object[size];
+        array = new Object[capacity];
     }
 
-    public DynamicArray(int size) {
-        this.size = size;
-        array = new Object[size];
+    public DynamicArray(int capacity) {
+        this.capacity = capacity;
+        array = new Object[capacity];
     }
 
-    @Override
-    public boolean add(Object o) {
-        if (actualSize == size) {
-            size = size * 2;
-            array = Arrays.copyOf(array,size);
+    public boolean add(T o) {
+        if (size == capacity) {
+            capacity = capacity * 2;
+            array = Arrays.copyOf(array, capacity);
         }
-        array[actualSize] = o;
-        actualSize++;
+        array[size] = o;
+        size++;
         return true;
     }
 
     @Override
     public int size() {
-        return actualSize;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return actualSize==0;
+        return size == 0;
     }
 
     @Override
     public boolean contains(Object o) {
-        return false;
+        return indexOf(o) >= 0;
     }
 
     @Override
@@ -60,60 +61,103 @@ public class DynamicArray<T> implements List {
 
     @Override
     public boolean remove(Object o) {
-        return false;
+        return remove(indexOf(o)) != null;
     }
 
     @Override
     public boolean addAll(Collection c) {
-        return false;
+        Object[] a = c.toArray();
+        int numNew = a.length;
+        ensureCapacityInternal(size + numNew);  // Increments modCount
+        System.arraycopy(a, 0, array, size, numNew);
+        size += numNew;
+        return numNew != 0;
     }
 
     @Override
     public boolean addAll(int index, Collection c) {
-        return false;
+        rangeCheckForAdd(index); //Почему нельзя сначала попробовать увеличить, а потом проверять уже возможность добавления
+
+        Object[] a = c.toArray();
+        int numNew = a.length;
+        ensureCapacityInternal(size + numNew);  // Increments modCount
+
+        int numMoved = size - index;
+        if (numMoved > 0)
+            System.arraycopy(array, index, array, index + numNew, numMoved);
+
+        System.arraycopy(a, 0, array, index, numNew);
+        size += numNew;
+        return numNew != 0;
     }
 
     @Override
     public void clear() {
+        for (int i = 0; i < size; i++)
+            array[i] = null;
 
+        size = 0;
     }
 
     @Override
-    public Object get(int index) {
-        return array[index];
+    public T get(int index) {
+        return elementData(index);
     }
 
     @Override
-    public Object set(int index, Object element) {
-        return null;
+    public T set(int index, Object element) {
+        array[index] = element;
+        return elementData(index);
     }
 
     @Override
     public void add(int index, Object element) {
-
+        rangeCheckForAdd(index);
+        ensureCapacityInternal(size + 1);
+        System.arraycopy(array, index, array, index + 1,
+                size - index);
+        array[index] = element;
+        size++;
     }
 
     @Override
-    public Object remove(int index) {
+    public T remove(int index) {
         //Не совсем понял каким образом в дефолтной реализации работает этот метод?
-        Object oldValue = array[index];
+        T oldValue = elementData(index);
         final int newSize;
-        if (index >= actualSize) {
+        if (index >= size) {
             return null;
         }
-        if ((newSize = actualSize - 1) > index)
+        if ((newSize = size - 1) > index)
             System.arraycopy(array, index + 1, array, index, newSize - index);
         return oldValue;
     }
 
-    @Override
     public int indexOf(Object o) {
-        return 0;
+        if (o == null) {
+            for (int i = 0; i < size; i++)
+                if (array[i] == null)
+                    return i;
+        } else {
+            for (int i = 0; i < size; i++)
+                if (o.equals(array[i]))
+                    return i;
+        }
+        return -1;
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        if (o == null) {
+            for (int i = size-1; i >= 0; i--)
+                if (array[i]==null)
+                    return i;
+        } else {
+            for (int i = size-1; i >= 0; i--)
+                if (o.equals(array[i]))
+                    return i;
+        }
+        return -1;
     }
 
     @Override
@@ -127,7 +171,7 @@ public class DynamicArray<T> implements List {
     }
 
     @Override
-    public List subList(int fromIndex, int toIndex) {
+    public List<T> subList(int fromIndex, int toIndex) {
         return null;
     }
 
@@ -150,4 +194,54 @@ public class DynamicArray<T> implements List {
     public Object[] toArray(Object[] a) {
         return new Object[0];
     }
+
+    T elementData(int index) {
+        return (T) array[index];
+    }
+
+    private static int calculateCapacity(Object[] array, int minCapacity) {
+        if (array == DEFAULT_CAPACITY_EMPTY_ARRAY) {
+            return Math.max(DEFAULT_CAPACITY, minCapacity);
+        }
+        return minCapacity;
+    }
+
+    private void ensureCapacityInternal(int minCapacity) {
+        ensureExplicitCapacity(calculateCapacity(array, minCapacity));
+    }
+
+    private void ensureExplicitCapacity(int minCapacity) {
+        if (minCapacity - array.length > 0)
+            grow(minCapacity);
+    }
+
+    private void grow(int minCapacity) {
+        // overflow-conscious code
+        int oldCapacity = array.length;
+        int newCapacity = oldCapacity + (oldCapacity >> 1);
+        if (newCapacity - minCapacity < 0)
+            newCapacity = minCapacity;
+        if (newCapacity - MAX_ARRAY_SIZE > 0)
+            newCapacity = hugeCapacity(minCapacity);
+        // minCapacity is usually close to size, so this is a win:
+        array = Arrays.copyOf(array, newCapacity);
+    }
+
+    private static int hugeCapacity(int minCapacity) {
+        if (minCapacity < 0) // overflow
+            throw new OutOfMemoryError();
+        return (minCapacity > MAX_ARRAY_SIZE) ?
+                Integer.MAX_VALUE :
+                MAX_ARRAY_SIZE;
+    }
+
+    private void rangeCheckForAdd(int index) {
+        if (index > size || index < 0)
+            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+    }
+
+    private String outOfBoundsMsg(int index) {
+        return "Index: " + index + ", Size: " + size;
+    }
+
 }
